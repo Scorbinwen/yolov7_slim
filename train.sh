13a82b43f25e4c0e256c5cd8cff9ad969fcb9463
Scorbinwen <scorbinwen@yahoo.com>
2025-08-21 13:48:45 +0800

    update train.sh

--- train.sh
+++ train.sh
@@ -3,7 +3,6 @@ python train.py --workers 8 --device "0,1,2,3,4,5,6,7" --batch-size 200 \
 --weights weights/yolov7.pt \
 --data data/tans.yaml --img 640 640 \
 --cfg cfg/training/yolov7.yaml \
---name yolov7 \
 --hyp data/hyp.scratch.p5.yaml --epochs 300 --name "train_v7_tans"
 ## ========================== train tans ========================== ##
 
@@ -12,7 +11,6 @@ python train.py --workers 8 --device "0,1,2,3,4,5,6,7" --batch-size 200 \
 --weights weights/yolov7.pt \
 --data data/pole_detection.yaml --img 640 640 \
 --cfg cfg/training/yolov7.yaml \
---name yolov7 \
 --hyp data/hyp.scratch.p5.yaml --epochs 300 --name "train_v7_pole_detection"
 ## ========================== train pole detection ========================== ##
 
@@ -22,6 +20,5 @@ python train.py --workers 8 --device "0,1,2,3,4,5,6,7" --batch-size 200 \
 --weights weights/yolov7.pt \
 --data data/combobox.yaml --img 640 640 \
 --cfg cfg/training/yolov7.yaml \
---name yolov7 \
 --hyp data/hyp.scratch.p5.yaml --epochs 300 --name "train_v7_combobox"
 ## ========================== train combobox ========================== ##
