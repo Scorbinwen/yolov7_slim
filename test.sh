e88110ac6364e30b618e84ccd2ad8b133935939b
Scorbinwen <scorbinwen@yahoo.com>
2025-08-22 10:34:33 +0800

    update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
-python test.py --data data/coco_slim_obj.yaml --img 640 --batch 32 --conf 0.001 --iou 0.75 --device 1 --weights weights/yolov7.pt --name yolov7_640_val
+python test.py --data data/tans.yaml --img 640 --batch 32 --conf 0.001 --iou 0.75 --device 1 --weights ./train_v7_tans/weights/best.pt --name val_tans
+python test.py --data data/pole_detection.yaml --img 640 --batch 32 --conf 0.001 --iou 0.75 --device 1 --weights ./train_v7_pole_detection/weights/best.pt --name val_pole_detection
+python test.py --data data/combobox.yaml --img 640 --batch 32 --conf 0.001 --iou 0.75 --device 1 --weights ./train_v7_combobox/weights/best.pt --name val_combobox
