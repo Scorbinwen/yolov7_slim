fcd03c6710a540de163f940bafc85ffe3528e4af
Scorbinwen <scorbinwen@yahoo.com>
2025-08-22 11:52:42 +0800

    update train.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-python test.py --data data/tans.yaml --img 640 --batch 32 --conf 0.001 --iou 0.75 --device 1 --weights ./runs/train_v7_tans/weights/best.pt --name val_tans
-python test.py --data data/pole_detection.yaml --img 640 --batch 32 --conf 0.001 --iou 0.75 --device 1 --weights ./runs/train_v7_pole_detection/weights/best.pt --name val_pole_detection
-python test.py --data data/combobox.yaml --img 640 --batch 32 --conf 0.001 --iou 0.75 --device 1 --weights ./runs/train_v7_combobox/weights/best.pt --name val_combobox
+python test.py --data data/tans.yaml --img 640 --batch 32 --conf 0.001 --iou 0.75 --device 0 --weights ./runs/train_v7_tans/weights/best.pt --name val_tans
+python test.py --data data/pole_detection.yaml --img 640 --batch 32 --conf 0.001 --iou 0.75 --device 0 --weights ./runs/train_v7_pole_detection/weights/best.pt --name val_pole_detection
+python test.py --data data/combobox.yaml --img 640 --batch 32 --conf 0.001 --iou 0.75 --device 0 --weights ./runs/train_v7_combobox/weights/best.pt --name val_combobox
--- train.sh
+++ train.sh
@@ -1,6 +1,5 @@
 ## ========================== train tans ========================== ##
 python train.py --workers 8 --device "0,1,2,3,4,5,6,7" --batch-size 200 \
---weights weights/yolov7.pt \
 --data data/tans.yaml --img 640 640 \
 --cfg cfg/training/yolov7.yaml \
 --hyp data/hyp.scratch.p5.yaml --epochs 300 --name "train_v7_tans"
@@ -8,7 +7,6 @@ python train.py --workers 8 --device "0,1,2,3,4,5,6,7" --batch-size 200 \
 
 ## ========================== train pole detection ========================== ##
 python train.py --workers 8 --device "0,1,2,3,4,5,6,7" --batch-size 200 \
---weights weights/yolov7.pt \
 --data data/pole_detection.yaml --img 640 640 \
 --cfg cfg/training/yolov7.yaml \
 --hyp data/hyp.scratch.p5.yaml --epochs 300 --name "train_v7_pole_detection"
@@ -17,7 +15,6 @@ python train.py --workers 8 --device "0,1,2,3,4,5,6,7" --batch-size 200 \
 
 ## ========================== train combobox ========================== ##
 python train.py --workers 8 --device "0,1,2,3,4,5,6,7" --batch-size 200 \
---weights weights/yolov7.pt \
 --data data/combobox.yaml --img 640 640 \
 --cfg cfg/training/yolov7.yaml \
 --hyp data/hyp.scratch.p5.yaml --epochs 300 --name "train_v7_combobox"
